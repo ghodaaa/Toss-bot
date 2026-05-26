600a528da630168944681c4fb884f5bdb719a57e
ghodaaa <aammaann841@gmail.com>
2026-05-26 20:06:58 +0530

    start menu

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { Telegraf } = require("telegraf");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
+const ADMIN_ID = Number(process.env.ADMIN_ID);
 
 const app = express();
 
@@ -9,8 +10,53 @@ app.get("/", (req, res) => {
   res.send("Bot Running");
 });
 
-bot.start((ctx) => {
-  ctx.reply("Toss Bot Active 🌝");
+bot.start(async (ctx) => {
+
+  let user = await User.findOne({
+    telegramId: ctx.from.id
+  });
+
+  if (!user) {
+
+    const count = await User.countDocuments();
+
+    user = await User.create({
+      telegramId: ctx.from.id,
+      username: ctx.from.username,
+      dpId: count + 1
+    });
+  }
+
+  const buttons = [];
+
+  buttons.push([
+    Markup.button.callback("🎯 Available Tosses", "tosses")
+  ]);
+
+  buttons.push([
+    Markup.button.callback("💰 Wallet", "wallet")
+  ]);
+
+  buttons.push([
+    Markup.button.callback("📜 My Bets", "mybets")
+  ]);
+
+  if (ctx.from.id === ADMIN_ID) {
+
+    buttons.push([
+      Markup.button.callback("⚙️ Admin Panel", "adminpanel")
+    ]);
+  }
+
+  ctx.reply(
+`🏏 Welcome ${ctx.from.first_name}
+
+🆔 DP ID: ${user.dpId}
+💰 Balance: ₹${user.balance}`,
+
+    Markup.inlineKeyboard(buttons)
+  );
+
 });
 
 bot.launch();
